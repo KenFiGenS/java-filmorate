b476094008ddfb899a366ace6b43e8ef81c73779
KenGa <sng.inc@yandex.ru>
2024-02-22 21:11:20 +0500

    fix: checkStyle 1.1, все тесты в Postman прошли успешно.

--- src/main/java/ru/yandex/practicum/filmorate/storage/BaseStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/BaseStorage.java
@@ -1,14 +1,10 @@
 package ru.yandex.practicum.filmorate.storage;
 
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import ru.yandex.practicum.filmorate.model.BaseUnit;
 
-import java.sql.SQLDataException;
-import java.sql.SQLException;
 import java.util.List;
 import java.util.Optional;
 
